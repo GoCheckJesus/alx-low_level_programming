6f4145cf1c59ccf367c775671ac1d1ee01621dfa
Sami SBAIHI <you@example.com>
2023-06-22 06:42:48 -0700

    so

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - draws straight line
+ * @n: check integer
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+	return (0);
+}
